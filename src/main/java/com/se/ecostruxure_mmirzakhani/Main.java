2bd218c25d49b582852e14147c3c2853819d2479
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-04-26 23:05:13 +0200

    Cleanup

--- src/main/java/com/se/ecostruxure_mmirzakhani/Main.java
+++ src/main/java/com/se/ecostruxure_mmirzakhani/Main.java
@@ -1,12 +1,5 @@
 package com.se.ecostruxure_mmirzakhani;
 
-import com.se.ecostruxure_mmirzakhani.be.Country;
-import com.se.ecostruxure_mmirzakhani.be.Employee;
-import com.se.ecostruxure_mmirzakhani.be.Region;
-import com.se.ecostruxure_mmirzakhani.be.Team;
-import com.se.ecostruxure_mmirzakhani.dal.EmployeeDAO;
-import com.se.ecostruxure_mmirzakhani.dal.db.DBConnection;
-import com.se.ecostruxure_mmirzakhani.exceptions.ExceptionHandler;
 import com.se.ecostruxure_mmirzakhani.utils.window.Window;
 import com.se.ecostruxure_mmirzakhani.utils.window.WindowType;
 
@@ -16,17 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     public static void main(String[] args) {
 
-        try {
-            EmployeeDAO dao = new EmployeeDAO();
-            System.out.println(dao.getEmployee(1));
-        } catch (ExceptionHandler e){
-            System.out.println(e.getMessage());
-        }
-
-
-
         Application.launch(args);
-
     }
 
 
