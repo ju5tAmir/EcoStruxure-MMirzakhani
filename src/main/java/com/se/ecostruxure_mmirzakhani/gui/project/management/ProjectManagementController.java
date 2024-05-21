d5131894857d62848b6554d53c541610e090adca
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-05-21 10:57:59 +0200

    New page and controller to manage a project with more details.

---
+++ src/main/java/com/se/ecostruxure_mmirzakhani/gui/project/management/ProjectManagementController.java
@@ -0,0 +1,87 @@
+package com.se.ecostruxure_mmirzakhani.gui.project.management;
+
+import com.se.ecostruxure_mmirzakhani.be.ProjectMember;
+import com.se.ecostruxure_mmirzakhani.bll.rate.RateService;
+import com.se.ecostruxure_mmirzakhani.gui.IController;
+import com.se.ecostruxure_mmirzakhani.model.Model;
+import com.se.ecostruxure_mmirzakhani.utils.CurrencyService;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ProjectManagementController implements IController<Model> {
+
+    @FXML
+    private Label totalEmployees, totalTeams, hourlyRate, dailyRate, productionCosts, overheadCosts, totalCosts;
+
+    @FXML
+    private TableView<ProjectMember> employeesTable;
+    @FXML
+    private TableColumn<ProjectMember, String> employeeFirstName;
+    @FXML
+    private TableColumn<ProjectMember, String> employeeLastName;
+    @FXML
+    private TableColumn<ProjectMember, String> employeeTeam;
+    @FXML
+    private TableColumn<ProjectMember, Double> employeeUtilPercentage;
+
+//    @FXML
+//    private TableView<>
+
+
+
+    @FXML
+    private Label projectName;
+
+    private Model model;
+    @Override
+    public void setModel(Model model) {
+        this.model = model;
+        projectName.setText(model.getProject().getName());
+
+        setLabels();
+        setEmployeeTable();
+
+    }
+
+    private void setEmployeeTable(){
+        employeesTable.setItems(model.getProjectMembers(model.getProject()));
+
+        employeeFirstName.setCellValueFactory(cellData -> {
+            String firstName = cellData.getValue().getEmployee().getFirstName();
+
+            return new SimpleStringProperty(firstName);
+        });
+
+        employeeLastName.setCellValueFactory(cellData -> {
+            String lastName = cellData.getValue().getEmployee().getLastName();
+
+            return new SimpleStringProperty(lastName);
+        });
+
+        employeeTeam.setCellValueFactory(cellData -> {
+            String teamName = cellData.getValue().getTeam().getName();
+
+            return new SimpleStringProperty(teamName);
+        });
+
+        employeeUtilPercentage.setCellValueFactory(cellData -> {
+            double utilizationPercentage = cellData.getValue().getUtilizationPercentage();
+
+            return new SimpleDoubleProperty(utilizationPercentage).asObject();
+        });
+    }
+
+
+    private void setLabels(){
+        totalEmployees.setText(String.valueOf(model.getProjectMembers(model.getProject()).size()));
+        totalTeams.setText(String.valueOf(model.getProjectTeams(model.getProject()).size()));
+        // We use stringFormatter method because we need to show it pretty in the GUI :D
+        hourlyRate.setText(CurrencyService.stringFormatter(model.getRate(model.getProject()).getHourlyRate()));
+        dailyRate.setText(CurrencyService.stringFormatter(model.getRate(model.getProject()).getDailyRate()));
+
+    }
+}
