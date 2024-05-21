a26acb5fdb6e39882c7d35ecbdf17b1a94e157fa
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-05-21 12:12:50 +0200

    Update RateService with get overhead/direct/total costs.

--- src/main/java/com/se/ecostruxure_mmirzakhani/bll/rate/RateService.java
+++ src/main/java/com/se/ecostruxure_mmirzakhani/bll/rate/RateService.java
@@ -77,10 +77,91 @@ public class RateService implements IRateService{
         return project;
     }
 
+    /**
+     * Get direct costs of a project (non-overhead or production resource)
+     */
+    public double getDirectCosts() {
+        // Initialize the total rate to accumulate the hourly rates of all employees
+        double totalRate = 0;
+
+        // if project doesn't have any member, return 0
+        if (projectMembers == null) {
+            return 0;
+        }
+
+        // Iterate through each project member to calculate
+        for (ProjectMember projectMember : projectMembers) {
+            Employee employee = projectMember.getEmployee();
+            // If the employee is NOT considered as overhead resource
+            if (!employee.getContract().isOverhead()) {
+                double employeeTotalCost = EmployeeService.getTotalCost(employee);
+                double employeeUtilizationPercentage = projectMember.getUtilizationPercentage() / 100; // Utilization percentage for this project of the employee
+
+                double cost = employeeTotalCost * employeeUtilizationPercentage;
+                totalRate += cost;
+            }
+        }
+
+        // Return the calculated cost as formatted
+        return CurrencyService.doubleFormatter(totalRate);
+    }
+
+    /**
+     * Get cost of overhead employees for the project
+     */
+    public double getOverheadCosts(){
+        // Initialize the total rate to accumulate the hourly rates of all employees
+        double totalRate = 0;
+
+        // if project doesn't have any member, return 0
+        if (projectMembers == null){
+            return 0;
+        }
+
+        // Iterate through each project member to calculate
+        for (ProjectMember projectMember : projectMembers) {
+            Employee    employee                        = projectMember.getEmployee();
+            // If the employee is considered as overhead resource
+            if (employee.getContract().isOverhead()) {
+                double employeeTotalCost = EmployeeService.getTotalCost(employee);
+                double employeeUtilizationPercentage = projectMember.getUtilizationPercentage() / 100; // Utilization percentage for this project of the employee
+
+                double cost = employeeTotalCost * employeeUtilizationPercentage;
+                totalRate += cost;
+            }
+        }
+
+        // Return the calculated cost as formatted
+        return CurrencyService.doubleFormatter(totalRate);
+    }
 
 
+    /**
+     * Get total costs: (direct + overhead) costs
+     * @return
+     */
+    public double getTotalCosts(){
+        // Initialize the total rate to accumulate the hourly rates of all employees
+        double totalRate = 0;
 
+        // if project doesn't have any member, return 0
+        if (projectMembers == null){
+            return 0;
+        }
 
+        // Iterate through each project member to calculate
+        for (ProjectMember projectMember : projectMembers) {
+            Employee    employee                        = projectMember.getEmployee();
+            double employeeTotalCost = EmployeeService.getTotalCost(employee);
+            double employeeUtilizationPercentage = projectMember.getUtilizationPercentage() / 100; // Utilization percentage for this project of the employee
+
+            double cost = employeeTotalCost * employeeUtilizationPercentage;
+            totalRate += cost;
+        }
+
+        // Return the calculated cost as formatted
+        return CurrencyService.doubleFormatter(totalRate);
+    }
 
 
     @Override
