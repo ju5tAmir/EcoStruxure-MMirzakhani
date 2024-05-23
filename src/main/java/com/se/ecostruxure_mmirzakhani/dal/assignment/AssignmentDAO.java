e4d922e759cd69d6a319d38bd5561a6775d219db
YehorNe1 <yehornechai1@gmail.com>
2024-05-24 00:22:52 +0200

    Assignment, Project

--- src/main/java/com/se/ecostruxure_mmirzakhani/dal/assignment/AssignmentDAO.java
+++ src/main/java/com/se/ecostruxure_mmirzakhani/dal/assignment/AssignmentDAO.java
@@ -1,4 +1,116 @@
 package com.se.ecostruxure_mmirzakhani.dal.assignment;
 
+import com.se.ecostruxure_mmirzakhani.be.entities.Assignment;
+import com.se.ecostruxure_mmirzakhani.dal.db.DBConnection;
+import com.se.ecostruxure_mmirzakhani.exceptions.ExceptionHandler;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssignmentDAO {
+
+    private final DBConnection dbConnection;
+
+    /**
+     * Constructor
+     */
+    public AssignmentDAO() throws ExceptionHandler {
+        dbConnection = new DBConnection();
+    }
+
+    public void createAssignment(int employeeId, int projectId, int teamId, float utilizationPercentage, String employeeType) throws SQLException {
+        String sql = "INSERT INTO Assignment (EmployeeID, ProjectID, TeamID, UtilizationPercentage, EmployeeType) VALUES (?, ?, ?, ?, ?)";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, employeeId);
+                statement.setInt(2, projectId);
+                statement.setInt(3, teamId);
+                statement.setFloat(4, utilizationPercentage);
+                statement.setString(5, employeeType);
+                statement.executeUpdate();
+            }
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Assignment getAssignment(int assignmentId) throws SQLException {
+        String sql = "SELECT * FROM Assignment WHERE AssignmentID = ?";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, assignmentId);
+                ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next()) {
+                    return new Assignment(
+                            resultSet.getInt("AssignmentID"),
+                            resultSet.getInt("EmployeeID"),
+                            resultSet.getInt("ProjectID"),
+                            resultSet.getInt("TeamID"),
+                            resultSet.getFloat("UtilizationPercentage"),
+                            resultSet.getString("EmployeeType"),
+                            resultSet.getTimestamp("FromDate"),
+                            resultSet.getTimestamp("ToDate")
+                    );
+                }
+            }
+            return null;
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Assignment> getAllAssignments() throws SQLException {
+        List<Assignment> assignments = new ArrayList<>();
+        String sql = "SELECT * FROM Assignment";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(sql);
+                while (resultSet.next()) {
+                    assignments.add(new Assignment(
+                            resultSet.getInt("AssignmentID"),
+                            resultSet.getInt("EmployeeID"),
+                            resultSet.getInt("ProjectID"),
+                            resultSet.getInt("TeamID"),
+                            resultSet.getFloat("UtilizationPercentage"),
+                            resultSet.getString("EmployeeType"),
+                            resultSet.getTimestamp("FromDate"),
+                            resultSet.getTimestamp("ToDate")
+                    ));
+                }
+            }
+            return assignments;
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateAssignment(int assignmentId, int employeeId, int projectId, int teamId, float utilizationPercentage, String employeeType) throws SQLException {
+        String sql = "UPDATE Assignment SET EmployeeID = ?, ProjectID = ?, TeamID = ?, UtilizationPercentage = ?, EmployeeType = ? WHERE AssignmentID = ?";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, employeeId);
+                statement.setInt(2, projectId);
+                statement.setInt(3, teamId);
+                statement.setFloat(4, utilizationPercentage);
+                statement.setString(5, employeeType);
+                statement.setInt(6, assignmentId);
+                statement.executeUpdate();
+            }
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAssignment(int assignmentId) throws SQLException {
+        String sql = "DELETE FROM Assignment WHERE AssignmentID = ?";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, assignmentId);
+                statement.executeUpdate();
+            }
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/se/ecostruxure_mmirzakhani/dal/project/ProjectDAO.java
+++ src/main/java/com/se/ecostruxure_mmirzakhani/dal/project/ProjectDAO.java
@@ -1,4 +1,92 @@
 package com.se.ecostruxure_mmirzakhani.dal.project;
 
+import com.se.ecostruxure_mmirzakhani.be.entities.Project;
+import com.se.ecostruxure_mmirzakhani.dal.db.DBConnection;
+import com.se.ecostruxure_mmirzakhani.exceptions.ExceptionHandler;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectDAO {
+
+    private final DBConnection dbConnection;
+
+    /**
+     * Constructor
+     */
+    public ProjectDAO() throws ExceptionHandler {
+        dbConnection = new DBConnection();
+    }
+
+    public void createProject(String projectName, String country) throws SQLException {
+        String sql = "INSERT INTO Projects (ProjectName, Country) VALUES (?, ?)";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, projectName);
+                statement.setString(2, country);
+                statement.executeUpdate();
+            }
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Project getProject(int projectId) throws SQLException {
+        String sql = "SELECT * FROM Projects WHERE ProjectID = ?";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, projectId);
+                ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next()) {
+                    return new Project(resultSet.getInt("ProjectID"), resultSet.getString("ProjectName"), resultSet.getString("Country"));
+                }
+            }
+            return null;
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Project> getAllProjects() throws SQLException {
+        List<Project> projects = new ArrayList<>();
+        String sql = "SELECT * FROM Projects";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(sql);
+                while (resultSet.next()) {
+                    projects.add(new Project(resultSet.getInt("ProjectID"), resultSet.getString("ProjectName"), resultSet.getString("Country")));
+                }
+            }
+            return projects;
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateProject(int projectId, String projectName, String country) throws SQLException {
+        String sql = "UPDATE Projects SET ProjectName = ?, Country = ? WHERE ProjectID = ?";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, projectName);
+                statement.setString(2, country);
+                statement.setInt(3, projectId);
+                statement.executeUpdate();
+            }
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteProject(int projectId) throws SQLException {
+        String sql = "DELETE FROM Projects WHERE ProjectID = ?";
+        try (Connection connection = dbConnection.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, projectId);
+                statement.executeUpdate();
+            }
+        } catch (ExceptionHandler e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
