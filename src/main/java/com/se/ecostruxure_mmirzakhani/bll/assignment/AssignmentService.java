4eceec0101168e9a1342355d63981be3252c475c
Ju5tAmir <amirbakhshizad53@gmail.com>
2024-05-24 20:48:24 +0200

    AssignmentService methods followed by IService.

--- src/main/java/com/se/ecostruxure_mmirzakhani/bll/assignment/AssignmentService.java
+++ src/main/java/com/se/ecostruxure_mmirzakhani/bll/assignment/AssignmentService.java
@@ -1,4 +1,34 @@
 package com.se.ecostruxure_mmirzakhani.bll.assignment;
 
-public class AssignmentService {
+import com.se.ecostruxure_mmirzakhani.be.entities.Assignment;
+import com.se.ecostruxure_mmirzakhani.bll.IService;
+import com.se.ecostruxure_mmirzakhani.dal.assignment.AssignmentDAO;
+import com.se.ecostruxure_mmirzakhani.exceptions.ExceptionHandler;
+
+public class AssignmentService implements IService<Assignment> {
+
+    private AssignmentDAO dao;
+    public AssignmentService(){
+        this.dao = new AssignmentDAO();
+    }
+
+    @Override
+    public void create(Assignment object) {
+
+    }
+
+    @Override
+    public void remove(Assignment object) throws ExceptionHandler{
+        dao.removeAssignment();
+    }
+
+    @Override
+    public void update(Assignment object) {
+
+    }
+
+    @Override
+    public void delete(Assignment object) {
+
+    }
 }
